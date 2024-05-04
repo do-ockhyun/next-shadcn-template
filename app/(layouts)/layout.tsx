a3f11467580cc3fa45696a41952a6485eace5cef
do-ockhyun <do-ockhyun@naver.com>
2024-05-05 08:41:41 +0900

    example to components

--- app/(layouts)/layout.tsx
+++ app/(layouts)/layout.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/layout/header";
 import Sidebar from "@/components/layout/sidebar";
+import { Button } from "@/components/ui/button";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -15,13 +16,31 @@ function DefaultLayout({
   children,
 }: DefaultLayoutProps) {
   return (
-    <>
-      <Header />
-      <div className="flex h-screen overflow-hidden">
-        <Sidebar />
-        <main className="w-full pt-16">{children}</main>
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+      <Sidebar />
+
+      <div className="flex flex-col">
+        <Header />
+
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+          <div className="flex items-center">
+            <h1 className="text-lg font-semibold md:text-2xl">Inventory</h1>
+          </div>
+          
+          <div className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm" x-chunk="dashboard-02-chunk-1">
+            <div className="flex flex-col items-center gap-1 text-center">
+              <h3 className="text-2xl font-bold tracking-tight">
+                You have no products
+              </h3>
+              <p className="text-sm text-muted-foreground">
+                You can start selling as soon as you add a product.
+              </p>
+              <Button className="mt-4">Add Product</Button>
+            </div>
+          </div>
+        </main>
       </div>
-    </>
+    </div>
   );
 }
 
--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,13 +1,19 @@
 import MobileSidebar from "./mobile-sidebar";
+import SearchBar from "./search-bar";
 import UserNav from "./user-nav";
 
 function Header() {
   return (
-    <>
+    <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
       <MobileSidebar />
-      <h1>Header</h1>
+
+      <div className="w-full flex-1">
+        <SearchBar />
+      </div>
+
       <UserNav />
-    </>);
+    </header>
+  );
 }
 
 export default Header;
